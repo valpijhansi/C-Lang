bc651aae3ab03f1df353e1fe60a47ea993b4ef0e
Valpi Jhansi <127584908+valpijhansi@users.noreply.github.com>
2023-10-05 19:54:49 +0530

    Assignment done

---
+++ Day2/FormttingSpecifiers.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+void main ()
+{
+    int x = 20,y = 30;
+    printf("%d%d\n", x,y);
+
+    int hours = 3,minutes = 20, seconds = 50;
+    printf("%d:%d:%d",hours,minutes,seconds);
+}
+
---
+++ Day2/sumOftwoNumbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int x,y;
+    printf("Enter the first number: ");
+    scanf("%d",&x);
+    printf("Enter second num: ");
+    scanf("%d",&y);
+    int z = x+y;
+    printf("%d+%d=%d",x,y,z);
+}
